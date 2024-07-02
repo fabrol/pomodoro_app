76fedbf1cc4b1a447036b9046f50110ad8d8512c
Farhan Abrol <fabrol92@gmail.com>
2024-07-02 00:17:47 -0400

    no console in production

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   reactStrictMode: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
